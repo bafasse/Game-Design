939d4fe5718f9546ff1baadf21708047665cacde
bafasse <blakefasse@gmail.com>
2018-08-04 16:28:15 -0500

    First Commit

---
+++ Unity2D/(2) Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Welcome to Number Wizard")	;
+
+		//Challenge
+		Debug.Log("Pick a number: ");
+		Debug.Log("The highest number you can pick is 1000");
+		Debug.Log("The lowest number you can pick is 1");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
