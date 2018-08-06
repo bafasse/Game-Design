4eadf9bbbb73fece7f70e6cc1dc06a29e10d6f4b
bafasse <blakefasse@gmail.com>
2018-08-06 16:49:07 -0500

    Finished video on understanding player response

--- Unity2D/(2) Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Unity2D/(2) Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -14,11 +14,22 @@ public class NumberWizard : MonoBehaviour {
 		Debug.Log("Pick a number: ");
 		Debug.Log("The highest number you can pick is " + max);
 		Debug.Log("The lowest number you can pick is " + min);
-
+		Debug.Log("Tell me if your number is higher or lower than 500");
+		Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			Debug.Log("You pressed the up arrow");
+		}
+
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			Debug.Log("You pressed the down arrow");
+		}
+
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			Debug.Log("You presses the Enter Key");
+		}
 	}
 }
