ba5bb07cd7bd802417b6e0f4d1d442bfae8e1be2
bafasse <blakefasse@gmail.com>
2018-08-06 17:09:21 -0500

    Tidied up code

--- Unity2D/(2) Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Unity2D/(2) Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -15,26 +15,27 @@ public class NumberWizard : MonoBehaviour {
 		Debug.Log("Pick a number: ");
 		Debug.Log("The highest number you can pick is " + max);
 		Debug.Log("The lowest number you can pick is " + min);
-		Debug.Log("Tell me if your number is higher or lower than 500");
+		Debug.Log("Tell me if your number is higher or lower than " + guess);
 		Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct!");
+		max = max + 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.UpArrow)) {
-			Debug.Log("You pressed the up arrow");
 			min = guess;
-			Debug.Log(guess);
+			guess = (max + min) / 2;
+			Debug.Log("Is you number higher or lower than " + guess);
 		}
 
 		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			Debug.Log("You pressed the down arrow");
 			max = guess;
-			Debug.Log(guess);
+			guess = (max + min) / 2;
+			Debug.Log("Is you number higher or lower than" + guess);
 		}
 
 		else if (Input.GetKeyDown(KeyCode.Return)) {
-			Debug.Log("You presses the Enter Key");
+			Debug.Log("This is the answer");
 		}
 	}
 }
