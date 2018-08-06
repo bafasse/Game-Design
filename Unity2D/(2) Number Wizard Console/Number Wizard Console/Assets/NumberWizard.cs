3e315e76d11b807e6680377d346d41a397ba50d6
bafasse <blakefasse@gmail.com>
2018-08-06 16:58:01 -0500

    Finished understanding scope of variables

--- Unity2D/(2) Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Unity2D/(2) Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
+	int max = 1000;
+	int min = 1;	
+	int guess = 500;
+
+
 	// Use this for initialization
 	void Start () {
-
-		int max = 1000;
-		int min = 1;
-
 		Debug.Log("Welcome to Number Wizard");
 		Debug.Log("Pick a number: ");
 		Debug.Log("The highest number you can pick is " + max);
@@ -22,13 +23,17 @@ public class NumberWizard : MonoBehaviour {
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.UpArrow)) {
 			Debug.Log("You pressed the up arrow");
+			min = guess;
+			Debug.Log(guess);
 		}
 
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
 			Debug.Log("You pressed the down arrow");
+			max = guess;
+			Debug.Log(guess);
 		}
 
-		if (Input.GetKeyDown(KeyCode.Return)) {
+		else if (Input.GetKeyDown(KeyCode.Return)) {
 			Debug.Log("You presses the Enter Key");
 		}
 	}
